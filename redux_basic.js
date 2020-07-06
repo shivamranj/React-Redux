ae9979e6e7f80c0945bd9d2ae01a26ed9a911fa3
Shivam <shivam@Shivams-MacBook-Air.local>
2020-07-06 23:57:14 +0530

    first commit

--- redux_basic.js
+++ redux_basic.js
@@ -6,12 +6,20 @@ const initialState={
 
 //Reducer
 
-function rootReducer(state=initialState,action)  
+const rootReducer=(state=initialState,action)  =>
 {
-    return state;
+    if(action.type="INC_counter")
+    {
+    counter: state.counter+1 
+       return counter
+    };
 };
 
 //store
 //reducer is paased in the store
 const store=redux.createStore(rootReducer)
 console.log(store.getState());
+
+//dispatching action
+store.dispatch({type: INC_counter})
+store.dispatch({type: DEC_counter,values:10})
